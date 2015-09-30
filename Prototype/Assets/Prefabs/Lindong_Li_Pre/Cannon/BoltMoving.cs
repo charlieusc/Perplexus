bff8db7529e78e549469c784e446cf79d95ec7f8
Lindong Li <lindongl@Lindongs-MacBook-Pro.local>
2015-09-30 11:26:50 -0700

    two prefabs

---
+++ Prototype/Assets/Prefabs/Lindong_Li_Pre/Cannon/BoltMoving.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltMoving : MonoBehaviour
+{
+	public float speed = 20.0f;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.up * speed;
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.CompareTag ("Bolt Terminal")) {
+			Destroy (gameObject);
+		}
+	}
+}
