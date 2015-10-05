60f6987aa258abb4adf963292ff794b927766598
charlieusc <jialinyang@JialindeMacBook-Pro.local>
2015-10-04 19:03:00 -0700

    deploy to iPad with 2 play scene

---
+++ Prototype/Assets/Scripts/EndGameTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameTrigger : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.name == "Ball") {
+			if (Application.loadedLevelName == "MariaTest") {
+				Application.LoadLevel ("Menu 3D");
+			} 
+			else {
+				Application.LoadLevel ("MariaTest");
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
--- Prototype/Assets/Scripts/RailScript.cs
+++ Prototype/Assets/Scripts/RailScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using VirtualController;
 
 public class RailScript : MonoBehaviour 
 {
@@ -26,12 +27,12 @@ public class RailScript : MonoBehaviour
 	void FixedUpdate () 
 	{
 		/*Movement Control Start*/
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		float moveSide = Input.GetAxis ("Side");
-		transform.RotateAround (Vector3.zero, Vector3.up, moveHorizontal);		
+		float moveHorizontal = Joysticks.GetAxis ("Horizontal");
+		float moveVertical = Joysticks.GetAxis ("Vertical");
+		float moveSide = Joysticks.GetAxis ("Row");
+		transform.RotateAround (Vector3.zero, Vector3.up, -moveHorizontal);		
 		transform.RotateAround (Vector3.zero, Vector3.right, moveVertical);
-		transform.RotateAround (Vector3.zero, Vector3.forward, moveSide);
+		transform.RotateAround (Vector3.zero, Vector3.forward, -moveSide);
 		/*Movement Control End*/
 	}
 }
